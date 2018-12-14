0c30f6fd242892caeee17bf8c6e8e895d8ff1134
Adam <adam.d.bell@icloud.com>
2018-12-14 12:06:44 -0800

    Update GreeterTest.java

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -11,7 +11,7 @@ public class GreeterTest {
 
 	@Test
 	public void greeterSaysHello() {
-		assertThat(greeter.sayHello(), containsString("Saul"));
+		assertThat(greeter.sayHello(), containsString("Hello"));
 	}
 
 }
